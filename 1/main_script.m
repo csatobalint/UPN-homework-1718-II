2c2f19b4fbec078795739652bd8b62c758cdab25
Bálint Csató <balintcsato@gmail.com>
2018-02-13 20:35:11 +0100

    Finished 2.0

--- 1/main_script.m
+++ 1/main_script.m
@@ -15,35 +15,35 @@ k=0.2;
 s=100;
 f0=60;
 w=2;
+F0=f0/s;
 
 %initial conditions
 c1=2;
 c2=2;
 
-%timestep
-%h=0.005;
-%N=(T-t0)/h;
 N=20000;
-
-
-%% forced vibration
-
-%[amp, amp_data, time] = fvbrRK4(T,N,m,k,s,f0,w,c1,c2)
-
+amp = fvbrRK4(T,N,m,k,s,f0,w,c1,c2)
 %% For loop for the amplification diagram
 
 ii=1;
-for w = 2:0.5:10
+for w = 0:0.5:14
     amp(ii)= fvbrRK4_amp(T,N,m,k,s,f0,w,c1,c2);
     w_list(ii)=w;
     ii=ii+1;
 end
 
+alfa=sqrt(s/m);
+D=k/(2*alfa*m);
+warr=linspace(0,14,10000);
+lambda=warr/alfa;
+N=(sqrt((1-lambda.^2).^2+4*D^2*lambda.^2)).^(-1);
+
 figure
+hold on
 plot(w_list/sqrt(s/m), amp*s/f0, '*')
 title('Amplification diagram')
 xlabel('$\lambda=\frac{\omega}{\alpha}$','interpreter','latex')
-xlabel('$N$','interpreter','latex')
-
-%%
-fvbrRK4(T,N,m,k,s,f0,w,c1,c2)
+ylabel('$N$','interpreter','latex')
+plot(lambda,N);
+ylim([0 80]) 
+hold off
